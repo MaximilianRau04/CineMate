ed1598ae95b3fa5ede84d84a50436b4c2146bc71
Maximilian Rau <maxi.rau2004@gmail.com>
2025-11-11 22:35:59 +0100

    updated tree generator

--- scripts/generateDirectory.js
+++ scripts/generateDirectory.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 const OUTPUT_FILE = "DIRECTORY.md";
 const ROOTS = (process.env.PATHS || "backend,frontend").split(",");
